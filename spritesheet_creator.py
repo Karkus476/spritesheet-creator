2759bfa6314e7fd18dba90b7290c63aa4db7b3ef
Karkus <karkus476@yahoo.com>
2015-10-10 23:23:22 +0100

    Recursively write to .stdx file

--- spritesheet_creator.py
+++ spritesheet_creator.py
@@ -1,4 +1,7 @@
 # A Script to generate a spritesheet and .stdx file
+# recursively from files in an input directory
+# Usage: 
+# python3 spritesheet_creator.py [-h] <input dir> <output image> <output text>
 # Copyright (C) 2015 Karkus476 <karkus476@yahoo.com>
 #
 # This program is free software: you can redistribute it and/or modify
@@ -14,58 +17,90 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import argparse, os
+import argparse, os, traceback
 from PIL import Image
 
 class Writer:
 	stdx = None #Output file.stdx
 	indentation = 0
-	def __init__(path):
+	def __init__(self, path):
 		self.stdx = open(path, 'w')
+		self.indentation = 0
 
-	def begin_dir(name):
-		self.stdx.write("\t"*indentation + name + " {")
+	def begin_dir(self, name):
+		self.stdx.write("\t"*self.indentation + name + " {\n")
 		self.indentation += 1
 	
-	def add_file(name, coords):
-		self.stdx.write("\t"*indentation + name + ":" + str(coords))
+	def add_file(self, name, coords):
+		self.stdx.write("\t"*self.indentation + name + ":" + str(coords)+"\n")
 
-	def end_dir():
+	def end_dir(self):
 		self.indentation -= 1
-		self.stdx.write("\t"*indentation + "}")
+		self.stdx.write("\t"*self.indentation + "}\n")
+		
+	def finish(self):
+		self.stdx.close()
 
 class ImageFile:
-	x = 0
-	y = 0
-	
-	filename = ""
 
-	image = None
+	@staticmethod
+	def from_path(filename, path):
+		try:
+			image = Image.open(path)
+		except IOError:
+			image = None
+		if image:
+			return ImageFile(filename, image)
+		else:
+			return None
 
-	def __init__(filename, image):
+	def __init__(self, filename, image):
 		self.filename = filename
 		self.image = image
+		
+		self.x = 0
+		self.y = 0
 	
-	def write(writer):
+	def write(self, writer):
 		writer.add_file(self.filename, self.get_loc())
 	
-	def get_loc():
-		return x, y
+	def get_loc(self):
+		return self.x, self.y
+
+	def set_loc(self, loc):
+		self.x = loc[0]
+		self.y = loc[1]
 
 class Directory:
-	name = ""
 
-	children = []
-
-	def __init__(name, children):
-		self.name = name
-		self.children = children
+	def __init__(self, path, dirname=None):
+		self.children = []
+		self.name = dirname
+		for name in os.listdir(path):
+			full_path = os.path.join(path, name)
+			if os.path.isfile(full_path):
+				image_file = ImageFile.from_path(name, full_path)
+				if image_file:
+					self.children.append(image_file)
+				else:
+					pass #file not image
+			elif os.path.isdir(full_path):
+				print("Directory:", full_path)
+				directory = Directory(full_path, name)
+				self.children.append(directory)
+			else:
+				print("Unexpected object in "+
+				      "path: "+full_path+" is "+
+				      "neither file nor directory.")
 	
-	def write(writer):
-		writer.begin_dir(self.name)
-		for child in children:
+	def write(self, writer):
+		#If name is None, then this is top dir so don't make new tag in file.
+		if self.name:
+			writer.begin_dir(self.name)
+		for child in self.children:
 			child.write(writer)
-		writer.end_dir()
+		if self.name:
+			writer.end_dir()
 	
 
 def load_all(path):
@@ -104,10 +139,15 @@ def start():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("base", help="Parent directory of images to add to spritesheet",type=str)
 	parser.add_argument("output", help="Output file path.",type=str)
+	parser.add_argument("index", help="Output .stdx path.",type=str)
 	args = parser.parse_args()
 	print("Loading all images from:", args.base)
 	
 	images = load_all(args.base)
+	base_dir = Directory(args.base)
+	writer = Writer(args.index)
+	base_dir.write(writer)
+	writer.finish()
 
 	#Sort images by height, largest first.
 	images = sorted(images, key=lambda val: val[1].size[1])
